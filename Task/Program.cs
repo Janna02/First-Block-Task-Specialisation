558f3fbdd1786a39bd8b7018e06b1dbe15a1bba5
Janna02 <dendy2@yandex.ru>
2022-11-23 17:32:43 +0300

    filter array on three chars

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,9 +2,6 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-Console.WriteLine("Задайте количество элементов в массиве: ");
-int numOfrRow = Convert.ToInt32(Console.ReadLine());
-
 string[] CreateArrayString(int num)
 {
     var array = new string[num];
@@ -23,7 +20,7 @@ void PrintArray(string[] arr)
     Console.Write("[");
     for (int i = 0; i < arr.Length; i++)
     {
-        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        if (i < arr.Length - 1) Console.Write($"{arr[i]} ");
         else Console.Write($"{arr[i]}");
     }
     Console.WriteLine("]");
@@ -31,17 +28,23 @@ void PrintArray(string[] arr)
 
 string[] FilterArrayThreeChars(string[] arr)
 {
-
-    for (int i = 0; i < arr.Length; i++)
+    var arrayFilter = new string[arr.Length];
+    for (int i = 0; i < arrayFilter.Length; i++)
     {
-        
-
+        if (arr[i].Length <= 3) arrayFilter[i] = arr[i];
     }
-    return arr;
+    return arrayFilter;
 }
 
+Console.WriteLine("Задайте количество элементов в массиве: ");
+int numOfrRow = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите значение элемента массива (строку из символов): ");
 string[] arrayOfRows = CreateArrayString(numOfrRow);
 Console.WriteLine("Первоначальный массив: ");
 PrintArray(arrayOfRows);
+
+
+string[] arrayFilter = FilterArrayThreeChars(arrayOfRows);
+Console.WriteLine("Отфильтрованный массив : ");
+PrintArray(arrayFilter);
